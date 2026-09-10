ca1df938275d3329c92fcca4463bbf3fcd8db66a
Karoll Mazabuel <karollmazabuel@unimayor.edu.co>
2026-09-09 22:56:19 -0500

    fix: agrega useStaticAssets para servir fotos de productos

--- vitrina-backend-nest/src/main.ts
+++ vitrina-backend-nest/src/main.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express'; // <-- NUEVO
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path'; 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule); 
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), { prefix: '/uploads' }); 
 
   const config = new DocumentBuilder()
     .setTitle('VitrinaApp API')
